284ba238414e2a79edf54442f179f6bd12c340bb
Maaz Bin Asad <60134184+maaz-bin-asad@users.noreply.github.com>
2020-03-31 19:05:59 +0530

    Delete Source.py

--- Source.py
+++
@@ -1,8 +0,0 @@
-from sklearn.preprocessing import LabelEncoder
-from sklearn.model_selection import train_test_split            #Importing the necessary libraries
-from sklearn.neighbors import KNeighborsClassifier
-import pandas as pd
-import numpy as np
-dataset=pd.read_csv('C:\Program Files\JetBrains\PyCharm Community Edition 2019.3.1\Covid19data.csv') #Reading the covid-19 dataset using pandas
-print(dataset.head())
-dataset=dataset.replace([np.inf,-np.inf],np.nan).dropna(axis=0)  #Cleaning the dataset to increase the performance
